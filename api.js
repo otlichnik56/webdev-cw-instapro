142610c8675a67a167cea2cce4e33b1b4ee9a45a
otlichnik56 <otlichnik56@yandex.ru>
2024-04-27 15:16:19 +0500

    Курсовая 2 от 27.04.2024

--- api.js
+++ api.js
@@ -4,6 +4,31 @@ const personalKey = "prod";
 const baseHost = "https://webdev-hw-api.vercel.app";
 const postsHost = `${baseHost}/api/v1/${personalKey}/instapro`;
 
+
+export function isLikedPost({ token, id, isLiked }) {
+  let host = "";
+  if (isLiked === true){
+    host = postsHost + "/" + id + "/dislike";
+  } else{
+    host = postsHost + "/" + id + "/like";
+  }
+  return fetch(host, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    }
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data.post;
+    });
+}
+
 export function getPosts({ token }) {
   return fetch(postsHost, {
     method: "GET",
@@ -15,11 +40,47 @@ export function getPosts({ token }) {
       if (response.status === 401) {
         throw new Error("Нет авторизации");
       }
-
       return response.json();
     })
     .then((data) => {
       return data.posts;
+    });
+}
+
+export function getPostsUser({ token, id }) {
+  return fetch(postsHost  + "/user-posts/" + id, {
+    method: "GET",
+    headers: {
+      Authorization: token,
+    },
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return data.posts;
+    });
+}
+
+export function postPost({ token, description, imageUrl }) {
+  return fetch(postsHost, {
+    method: "POST",
+    headers: {
+      Authorization: token,
+    },
+    body: JSON.stringify({
+      description: description,
+      imageUrl: imageUrl
+    }),
+  })
+    .then((response) => {
+      if (response.status === 401) {
+        throw new Error("Нет авторизации");
+      }
+      return response.json();
     });
 }
 
--- components/upload-image-component.js
+++ components/upload-image-component.js
@@ -5,29 +5,28 @@ export function renderUploadImageComponent({ element, onImageUrlChange }) {
 
   const render = () => {
     element.innerHTML = `
-  <div class="upload=image">
-      ${
-        imageUrl
-          ? `
-          <div class="file-upload-image-conrainer">
-            <img class="file-upload-image" src="${imageUrl}">
-            <button class="file-upload-remove-button button">Заменить фото</button>
-          </div>
+      <div class="upload=image">
+          ${
+            imageUrl
+              ? `
+              <div class="file-upload-image-conrainer">
+                <img class="file-upload-image" src="${imageUrl}">
+                <button class="file-upload-remove-button button">Заменить фото</button>
+              </div>
+              `
+              : `
+                <label class="file-upload-label secondary-button">
+                    <input
+                      type="file"
+                      class="file-upload-input"
+                      style="display:none"
+                    />
+                    Выберите фото
+                </label>              
           `
-          : `
-            <label class="file-upload-label secondary-button">
-                <input
-                  type="file"
-                  class="file-upload-input"
-                  style="display:none"
-                />
-                Выберите фото
-            </label>
-          
-      `
-      }
-  </div>
-`;
+          }
+      </div>
+    `;
 
     const fileInputElement = element.querySelector(".file-upload-input");
 
@@ -39,6 +38,7 @@ export function renderUploadImageComponent({ element, onImageUrlChange }) {
         lableEl.textContent = "Загружаю файл...";
         uploadImage({ file }).then(({ fileUrl }) => {
           imageUrl = fileUrl;
+          //console.log(imageUrl);
           onImageUrlChange(imageUrl);
           render();
         });
